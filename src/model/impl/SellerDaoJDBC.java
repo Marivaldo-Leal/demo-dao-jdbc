a3dd8e590ef0acac4ca85e9f87e8e3aa56e4b40e
Ma-Leal <marivaldo-@hotmail.com>
2025-06-11 10:22:36 -0300

    feat(seller): add findById implementation

--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+    private final Connection conn;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
     @Override
     public void insert(Seller obj) {
 
@@ -23,7 +36,39 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            ps = conn.prepareStatement(
+                    "select s.*, d.Name as depName "
+                            + "from seller s inner join department d on s.DepartmentId = d.Id "
+                            + "where s.id = ?");
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Department department = new Department();
+                department.setId(rs.getInt("DepartmentId"));
+                department.setName(rs.getString("depName"));
+
+                Seller seller = new Seller();
+                seller.setId(rs.getInt("id"));
+                seller.setName(rs.getString("Name"));
+                seller.setEmail(rs.getString("Email"));
+                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                seller.setBirthDate(rs.getDate("BirthDate").toLocalDate());
+                seller.setDepartment(department);
+
+                return seller;
+
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(ps);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
--- src/program/App.java
+++ src/program/App.java
@@ -1,24 +1,13 @@
 package program;
 
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class App {
     public static void main(String[] args) {
-
-        Department d = new Department(1, "Electronics");
-        Seller seller = new Seller(
-                1,
-                "mario",
-                "mario@gmail.com",
-                LocalDate.parse("01/01/2000", DateTimeFormatter.ofPattern("dd/MM/yyyy")),
-                2500.00,
-                d
-        );
-
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
