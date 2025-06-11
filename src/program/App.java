270e893b9c5dfdcf94ae345af66e6ca1f2b7da40
Ma-Leal <marivaldo-@hotmail.com>
2025-06-10 21:27:28 -0300

    feat: add entitie Department

---
+++ src/program/App.java
@@ -0,0 +1,10 @@
+package program;
+
+import model.entities.Department;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(new Department(1, "Electronics"));
+    }
+}
